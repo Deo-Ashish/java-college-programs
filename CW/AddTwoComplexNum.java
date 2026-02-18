10260c62244dc6a790389d7902f856f2b5d74595
Deo-Ashish <deoashish942@gmail.com>
2026-02-18 23:45:50 +0530

    Program for addition of two complex number

---
+++ CW/AddTwoComplexNum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class AddTwoComplexNum {
+    int r, i;
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter real and imaginary numbers: ");
+        r = sc.nextInt();
+        i = sc.nextInt();
+    }
+
+    void sum(AddTwoComplexNum C2) {
+        r = r + C2.r;
+        i = i + C2.i;
+    }
+
+    void display() {
+        System.out.println("Real = " + r);
+        System.out.println("Imaginary = " + i);
+    }
+}
+
+class complex1 {
+    public static void main(String[] args) {
+        AddTwoComplexNum C1 = new AddTwoComplexNum();
+        AddTwoComplexNum C2 = new AddTwoComplexNum();
+        C1.getData();
+        C2.getData();
+        C1.sum(C2);
+        C1.display();
+    }
+}
